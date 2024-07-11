807ba26eb4dbd405326126a7cb18feca8b4d8054
Md Salauddin Sakib <salauddinsakib99@gmail.com>
2024-07-11 14:21:05 +0600

    screen hidden and visible added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,10 @@
 function play(){
-    console.log('play start now');
+    
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden');
+    // console.log(homeSection.classList);
+
+    const playGroundSection = document.getElementById('play-ground');
+    // console.log(playGroundSection.classList);
+    playGroundSection.classList.remove('hidden');
 }
